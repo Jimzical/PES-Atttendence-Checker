11bd7fcb3ad82f047847046a9131b2759b0115e3
Arish Kumar <97384467+Jimzical@users.noreply.github.com>
2023-01-24 23:08:58 +0530

    Update Attednec V6.py
    added better console logs

--- Attednec V6.py
+++ Attednec V6.py
@@ -1,13 +1,13 @@
 # V6
-# Remember Me Functionality Added
-# Fixed the TryElement Function
-# Fixed Fonts
-# Fixed the Loading Window
-# Added Default Username and Password
-# Added json file to store the username and password
-# Added Error Handling for Wrong Username and Password
-# Added Error Handling for Internet Connection
-# Added Timeout Limit for the Webpage to Load
+# - Remember Me Functionality Added
+# - Fixed the TryElement Function
+# - Fixed Fonts
+# - Fixed the Loading Window
+# - Added Default Username and Password
+# - Added json file to store the username and password
+# - Added Error Handling for Wrong Username and Password
+# - Added Error Handling for Internet Connection
+# - Added Timeout Limit for the Webpage to Load
 
 
 from selenium import webdriver
@@ -29,6 +29,8 @@ def buildGUI():
     '''
 
     # read secrets.json file
+    print('Reading Files....')
+
     if os.path.exists('secrets.json'):
         with open('secrets.json') as f:
             data = json.load(f)
@@ -41,6 +43,7 @@ def buildGUI():
             DefaultUsername = ''
             DefaultPassword = ''
     
+    print('Reading Files....Done')
 
 
     # sg.theme('Black')   # Add a touch of color
@@ -53,22 +56,21 @@ def buildGUI():
     window = sg.Window('Attendence Calculator',layout = layout,grab_anywhere=True)
     event, values = window.read()
 
+    print('Waiting for the User to Enter the Credentials....')
 
     if event == 'Submit':
+        print('Credentials Entered....')
         if (values['-REMEMBER-'] and values['-USERNAME-'] != '' or values['-PASSWORD-'] != '' ):
             data['Username'] = values['-USERNAME-']
             data['Password'] = values['-PASSWORD-']
             with open('secrets.json','w') as f:
                 json.dump(data,f)
         
-        window.close()
-        print(values['-USERNAME-'])
-        print(values['-PASSWORD-'])
-        print(values['-ATTENDENCE-'])
-        
+        window.close()        
         main(username = values['-USERNAME-'],password = values['-PASSWORD-'])
     
-    elif event == 'Cancel':
+    elif event == 'Cancel' or event == sg.WIN_CLOSED:
+            print('Cancelled')
             window.close()
             browser.close()
             exit()
@@ -160,6 +162,7 @@ def Scraping(browser,username,password):
     # Login
     
     # TODO: Fix the TryElement function
+    print('Logging In....')
     user = TryElement(browser,"xpath", '//*[@id= "j_scriptusername" ]',action= "Input")
     pwd = TryElement(browser,"xpath", '/html/body/div[2]/div[1]/div/div[2]/form/fieldset/div[2]/input',action= "Input")
     
@@ -169,12 +172,14 @@ def Scraping(browser,username,password):
     # Submit Button
     TryElement(browser,"xpath", '//*[@id="postloginform#/Academy/j_spring_security_check"]',action= 'click')
 
+    print('Logged In')
+
+    print('Getting the Data....')
     # Attendece Menu
     TryElement(browser,"xpath", '//*[@id="menuTab_660"]/a/span[2]',action= 'click')
 
     # Getting Info
     table = TryElement(browser,"xpath", '//*[@id="subjetInfo"]',action= 'text')
-    
     item = table.split("\n")
 
     print()
@@ -194,6 +199,8 @@ def Scraping(browser,username,password):
 
     df = pd.DataFrame(ClassesAndPercentrage,columns=["Classes","Percentage"],index = subjectName)
     df.index.name = "Subject"
+
+    print('Data Scraped')
     return df
 
 # Attendence Calcuation
@@ -212,6 +219,8 @@ def attendence(Subject ,Classes = '0/0', attendence_limit = 80, Percents = 0):
     Returns:
         stringItem: [str]   
     '''
+    print('Calculating Attendence....')
+
     if type(attendence_limit) == str:
         attendence_limit = 80
     attendence_limit = int(attendence_limit)
@@ -247,6 +256,8 @@ def attendence(Subject ,Classes = '0/0', attendence_limit = 80, Percents = 0):
     stringItem += f"Min No of Classes to be Attended {classesAttended}\nNo of Classes it will take Totally {classesTaken}\n"
     
     stringItem += f"Percentage: {round(classesAttended/classesTaken*100,2)}\n"
+
+    print('Attendence Calculated')
     return stringItem
 
 def ShowData(df,TotalData,LoadingWindow,browser):
@@ -266,6 +277,11 @@ def ShowData(df,TotalData,LoadingWindow,browser):
     '''
     browser.close()
 
+    print('Closing the Browser....')
+
+    print('Showing the Data....')
+    print('Building the Window....')
+
     out = df.to_markdown(tablefmt='pipe', colalign=['center']*len(df.columns))
     
     col = [sg.Column([[sg.Text(out,font=('Courier'))],[sg.Text(TotalData,font=('Courier'))],[sg.Button('Ok',auto_size_button=True,font= ('Courier'))]],scrollable=True, element_justification='center',vertical_scroll_only=True)]
@@ -274,9 +290,16 @@ def ShowData(df,TotalData,LoadingWindow,browser):
     newWindow = sg.Window('Data',layout=layout,resizable=True)
     LoadingWindow.close()
     event, values = newWindow.read()
+    
+    print('Window Built')
+
+    print('Data Shown')
 
     if event == 'Ok' or event == sg.WIN_CLOSED:
         newWindow.close()
+        print('Exiting....')
+        print('Exited')
+        print('Thank You')
         exit()
 
 def main(username,password,attnLimit = 80):
@@ -294,25 +317,27 @@ def main(username,password,attnLimit = 80):
         None
     '''
     global LoadingWindow
+    
+    print("Loading....")
+
     LoadingWindow = sg.Window('Loading',[[sg.Text('Loading....',font=("Casadia Code",15))]])
     LoadingWindow.read(timeout=0)
 
+    print("Opening Browser")
     browser = OpenBrowser()
 
-    df = Scraping(browser=browser,username=username,password=password)
 
-    print()
-    print(df)
-    print()
+    df = Scraping(browser=browser,username=username,password=password)
 
     # attendence(Classes = (df.loc[row[0],"Classes"]), attendence_limit = 80)
     TotalData = ""
     for ind in df.index:
             text = attendence(Subject = ind,Classes = df["Classes"][ind], attendence_limit = attnLimit, Percents = df["Percentage"][ind])
-            print(text)
             TotalData += text
 
     ShowData(df,TotalData,LoadingWindow,browser)
 
 if __name__ == "__main__":
+    print('Starting....')
+    print('Building GUI')
     buildGUI()
